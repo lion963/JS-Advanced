f4b86a3d3457a3f50b84bf97742f5085195dd677
Asen Kamboshev <lion963@mail.bg>
2021-08-09 15:20:53 +0300

    test

--- Syntax/MathOperations.js
+++ Syntax/MathOperations.js
@@ -1,13 +1,13 @@
-function math_op(num1, num2, operator){
+function math_op(num1, num2, operator) {
     let result;
-    // switch (operator){
-    //     case '+': result = num1+num2; break;
-    //     case '-': result = num1-num2; break;
-    //     case '/': result = num1/num2; break;
-    //     case '*': result = num1*num2; break;
-    //     case '%': result = num1%num2; break;
-    //     case '**': result = num1**num2; break;
-    // }
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '/': result = num1 / num2; break;
+        case '*': result = num1 * num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
     console.log(result)
 }
 
