95f95e1494400197aed12f48caf6efae16217f28
Asen Kamboshev <lion963@mail.bg>
2021-09-01 16:14:55 +0300

    01.09

--- ObjectsAndComposition/JSONtoHTML.js
+++ ObjectsAndComposition/JSONtoHTML.js
@@ -5,26 +5,47 @@ function JsonToHtmlTable(json){
     arr.forEach((obj) => outputArr.push(makeValueRow(obj)));
     outputArr.push("</table>");
 
-    function makeKeyRow(arr){}
-    function makeValueRow(obj){}
-    function escapeHtml(value){}
+    function makeKeyRow(arr){
+        let KeyRow = [];
+        let keys = Object.keys(arr[0]);
+        keys.forEach((key, i) => keys[i]=escapeHtml(key));
+        keys.forEach(key => KeyRow.push("<th>"+key+"</th>"))
+        KeyRow.unshift("    <tr>");
+        KeyRow.push("</tr>")
+        return KeyRow.join('')
+    }
+
+    function makeValueRow(obj){
+        let ValueRow = [];
+        let values = Object.values(obj);
+        values.forEach((value, i) => values[i]=escapeHtml(value));
+        values.forEach(value => ValueRow.push("<td>"+value+"</td>"))
+        ValueRow.unshift("    <tr>")
+        ValueRow.push("</tr>")
+        return ValueRow.join('')
+
+    }
+
+    function escapeHtml(value){
+        return escape(value)
+    }
 
     console.log(outputArr.join('\n'));
 }
 
 
-`[{"Name":"Stamat",
+JsonToHtmlTable(`[{"Name":"Stamat",
     "Score":5.5},
    {"Name":"Rumen",
-    "Score":6}]`
+    "Score":6}]`)
 
 
-`[{"Name":"Pesho",
+    JsonToHtmlTable(`[{"Name":"Pesho",
     "Score":4,
-    " Grade":8},
+    "Grade":8},
    {"Name":"Gosho",
     "Score":5,
     " Grade":8},
    {"Name":"Angel",
     "Score":5.50,
-    " Grade":10}]`
+    "Grade":10}]`)
